40955f747c6c23c49fe856d997b732fec157a46a
jannatunn <128748428+jannatunn@users.noreply.github.com>
2024-11-01 11:34:24 +0700

    add pagination, footer

--- src/components/atoms/Image.jsx
+++ src/components/atoms/Image.jsx
@@ -1,8 +1,9 @@
 // src/components/atoms/Image.js
 import React from 'react';
 
-const Image = ({ src, alt, style }) => {
-  return <img src={src} alt={alt} style={style} />;
-};
+const Image = ({ src, alt, onLoad, className }) => (
+  <img src={src} alt={alt} onLoad={onLoad} className={className} />
+);
+
 
 export default Image;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,22 +1,60 @@
-import { useState } from 'react';
+// src/pages/HomePage.js
+import React, { useState, useEffect } from 'react';
 import MainLayout from '../templates/MainLayout';
 import VideoContainer from '../organisms/VideoContainer';
+import SortSelect from '../molecules/SortSelect';
+import Pagination from '../molecules/Pagination';
 import data from '../../data/fetchVideos';
+import Telegram from '../atoms/Telegram';
 
 const HomePage = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const fileList = data.result.files;
+  const [fileList, setFileList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
 
-  const filteredList = fileList.filter(video =>
-    video.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  useEffect(() => {
+    setFileList(data.result.files);
+  }, []);
+
+  const sortVideos = (criteria) => {
+    let sortedVideos = [...fileList];
+
+    if (criteria === 'popular') {
+      sortedVideos.sort((a, b) => b.views - a.views);
+    } else if (criteria === 'latest') {
+      sortedVideos.sort((a, b) => new Date(b.uploaded) - new Date(a.uploaded));
+    } else if (criteria === 'oldest') {
+      sortedVideos.sort((a, b) => new Date(a.uploaded) - new Date(b.uploaded));
+    }
+
+    setFileList(sortedVideos);
+  };
+
+  const handleSortChange = (event) => {
+    sortVideos(event.target.value);
+  };
+
+  // Calculate current items
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = fileList.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const totalPages = Math.ceil(fileList.length / itemsPerPage);
 
   return (
-    <MainLayout searchTerm={searchTerm} setSearchTerm={setSearchTerm}>
-      <h3>Kawasan Jilbob</h3>
-      <h1>Account Info</h1>
-      {filteredList.length > 0 ? (
-        <VideoContainer fileList={filteredList} />
+    <MainLayout>
+      <div className="flex justify-between items-center my-2">
+        <SortSelect onSortChange={handleSortChange} />
+        <Telegram />
+      </div>
+      {currentItems.length > 0 ? (
+        <>
+          <VideoContainer fileList={currentItems} />
+          <Pagination totalPages={totalPages} currentPage={currentPage} paginate={paginate} />
+        </>
       ) : (
         <p>No videos available.</p>
       )}
